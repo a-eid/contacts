da9a230b67127523d79979f7a04874e457bfb71c
Ahmed Eid <a.eid@yandex.com>
2018-04-08 08:14:14 +0200

    dynamic sections

--- Contacts/Home.swift
+++ Contacts/Home.swift
@@ -3,6 +3,16 @@ import ios_essentials
 
 class Home: UITableViewController {
   let nCell = "nCell"
+  let names = [
+    ["one", "two", "three"],
+    ["four", "five", "six", "what"],
+    ["seven", "eight", "nine", "ten", "tweleve"]
+  ]
+  
+  let headerTitles = [
+    "first", "second", "third"
+  ]
+
   override func viewDidLoad() {
     super.viewDidLoad()
     navigationItem.title = "Contacts"
@@ -14,21 +24,21 @@ class Home: UITableViewController {
   override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
     let l = UILabel()
     l.backgroundColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
-    l.text = "header"
+    l.text = headerTitles[section]
     return l
   }
   
   override func numberOfSections(in tableView: UITableView) -> Int {
-    return 2
+    return names.count
   }
   
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 10
+    return names[section].count
   }
   
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCell(withIdentifier: nCell, for: indexPath)
-    cell.textLabel?.text = "\(indexPath.row)"
+    cell.textLabel?.text = names[indexPath.section][indexPath.row]
     return cell
   }
 
